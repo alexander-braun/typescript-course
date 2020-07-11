a72ec1d0abda63bc19c513753a7eb3ef21095e04
alexander-braun <alexanderbraun@freenet.de>
2020-07-11 23:55:02 +0200

    classes and interfaces done

---
+++ 2/interfaces.js
@@ -0,0 +1,25 @@
+"use strict";
+var add;
+add = function (n1, n2) {
+    return n1 + n2;
+};
+console.log(add(2, 2));
+var Person = /** @class */ (function () {
+    function Person(n) {
+        this.age = 30;
+        if (n) {
+            this.name = n;
+        }
+    }
+    Person.prototype.greet = function (phrase) {
+        if (this.name) {
+            console.log(phrase + ' ' + this.name);
+        }
+        else {
+            console.log('Hi!');
+        }
+    };
+    return Person;
+}());
+var user1 = new Person();
+user1.greet("Hey there, I'm");
